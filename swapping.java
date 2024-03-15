cc046d52b1d22b24206fcffee1cd160da4638add
Harshivarma <95752442+Harshivarma@users.noreply.github.com>
2024-03-15 15:49:25 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Swapping
+{
+    public static void main(String args[])
+    {
+        int a=200;
+        int b=300;
+        System.out.println("Values of a and b are (before swapping) : "+a+" "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Values of a and b are (after swapping) : "+a+" "+b);
+    }
+}
+
+  
